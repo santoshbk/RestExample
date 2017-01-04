6ef4ea2f3673cee1b768d45387101700dcac4dac
Santosh K <bornfree.kushi@gmail.com>
2017-01-04 14:33:45 +0000

    Test From Codenvy

--- RestAPI/src/main/java/com/santosh/web/RestApiApplication.java
+++ RestAPI/src/main/java/com/santosh/web/RestApiApplication.java
@@ -1,5 +1,8 @@
 package com.santosh.web;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,5 +11,18 @@ public class RestApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApiApplication.class, args);
+	    InetAddress ip;
+        String hostname;
+        try {
+            ip = InetAddress.getLocalHost();
+            hostname = ip.getHostName();
+            System.out.println("Your current IP address : " + ip);
+            System.out.println("Your current Hostname : " + hostname);
+ 
+        } catch (UnknownHostException e) {
+ 
+            e.printStackTrace();
+        }
+	    	   
 	}
 }
